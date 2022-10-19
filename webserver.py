2aa0b4cd6b912b681bf60bc2711f496e5316e9c6
Ed Paradis <eparadis@users.noreply.github.com>
2022-10-18 23:23:07 -0700

    fix json response format

--- webserver.py
+++ webserver.py
@@ -14,10 +14,10 @@ async def ping(request):
 
 @naw.route("/data.json")
 async def data(request):
-    await request.write("HTTP/1.1 200 OK\r\n\r\n")
+    await request.write("HTTP/1.1 200 OK\r\n")
     await request.write("Content-Type: application/json\r\n\r\n")
     sensor.measure() # it's probably not wise to have this blocking here. instead we should poll the sensor and this should simply report the most recent value and date stamp or some such
-    await request.write('{"temp": %f, "RH": %f}' % (sensor.temperature(), sensor.humidity()))
+    await request.write('{"temp": %f, "RH": %f}\r\n' % (sensor.temperature(), sensor.humidity()))
 
 loop = asyncio.get_event_loop()
 loop.create_task(naw.run())
