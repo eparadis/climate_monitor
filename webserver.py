0c097745e4b67f7cea84dd7ba20008b23ebf3d78
Ed Paradis <eparadis@users.noreply.github.com>
2022-10-19 20:22:17 -0700

    do fahrenheit conversion on server because gardening is all in imperial units anyhow

--- webserver.py
+++ webserver.py
@@ -7,6 +7,9 @@ sensor = DHT22(Pin(14))
 
 naw = Nanoweb(80)
 
+def c_to_f(c):
+  return c * 9.0 / 5.0 + 32
+
 @naw.route("/ping")
 async def ping(request):
     await request.write("HTTP/1.1 200 OK\r\n\r\n")
@@ -17,7 +20,7 @@ async def data(request):
     await request.write("HTTP/1.1 200 OK\r\n")
     await request.write("Content-Type: application/json\r\n\r\n")
     sensor.measure() # it's probably not wise to have this blocking here. instead we should poll the sensor and this should simply report the most recent value and date stamp or some such
-    await request.write('{"temp": %f, "RH": %f}\r\n' % (sensor.temperature(), sensor.humidity()))
+    await request.write('{"temp": %f, "RH": %f}\r\n' % (c_to_f(sensor.temperature()), sensor.humidity()))
 
 loop = asyncio.get_event_loop()
 loop.create_task(naw.run())
