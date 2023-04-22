70b4242c040d00d917ec2aada0e34e82e6da0d3b
Ed Paradis <eparadis@users.noreply.github.com>
2023-04-22 15:03:45 -0700

    bring over tools from climate_display

---
+++ connect.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 ~/git/webrepl/webrepl_cli.py  -p `cut -d\' -f2 webrepl_cfg.py` 192.168.0.111
---
+++ send_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# if no argument is given, report usage
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+python3 ~/git/webrepl/webrepl_cli.py  -p `cut -d\' -f2 webrepl_cfg.py` $1 192.168.0.111:
